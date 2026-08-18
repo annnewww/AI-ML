039cbacd88cee267b7001000a59021180f340063
Anurag Shukla <110663904+annnewww@users.noreply.github.com>
2026-08-18 13:07:58 +0530

    Update backup_brim_ai_tool.py

--- backup_brim_ai_tool.py
+++ backup_brim_ai_tool.py
@@ -21,7 +21,7 @@ load_dotenv()
 
 # --- MODEL & PATH CONFIGURATIONS ---
 #model_Name = "llama-3.3-70b-versatile"
-#model_Name = "openai/gpt-oss-120b"
+model_Name = "openai/gpt-oss-120b"
 model_Name_bkp = "llama-3.1-8b-instant"
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
